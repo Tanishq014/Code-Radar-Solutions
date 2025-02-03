2b8cecc57e01bdc324049a0c94e74ea0375a06ba
Tanishq014 <tanishq130805@gmail.com>
2025-02-03 18:06:17 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -8,10 +8,10 @@ int main() {
     int a,b;
     char c;
     scanf("%d%d%c",&a,&b,&c);
-    if(c=='+'){printf("d",a+b);}
-    if(c=='-'){printf("d",a-b);}
-    if(c=='*'){printf("d",a*c);}
-    if(c=='/'){printf("d",a/c);}
+    if(c=='+'){printf("%d",a+b);}
+    if(c=='-'){printf("%d",a-b);}
+    if(c=='*'){printf("%d",a*c);}
+    if(c=='/'){printf("%d",a/c);}
 
     return 0;
 }
