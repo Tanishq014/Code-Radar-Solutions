90b61084cfded9ac0150eb752deeabe0c2d95be4
Tanishq014 <tanishq130805@gmail.com>
2025-02-03 18:55:09 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+}
+
+int main() {
+    char a;
+    scanf("%c",&a);
+    switch(a){
+        case A:printf("Excellent");break;
+        case B:printf("Good");break;
+        case C:printf("Average");break;
+        case D:printf("Below Average");break;
+        case E:printf("Fail");break;
+    }
+    return 0;
+}
