dcf8e3eef29bc9cd24053ac1a2f58865202fc1b2
Tanishq Tanishq <tanishq130805@gmail.com>
2025-03-20 18:35:20 +0530

    Solution to Check if Two Strings are Rotations of Each Other

--- Check if Two Strings are Rotations of Each Other@@67877c59ddaeab38c8ad25ae/code.c
+++ Check if Two Strings are Rotations of Each Other@@67877c59ddaeab38c8ad25ae/code.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
-int main() {
-    char s1[200], s2[100]; // Increased size of s1 to handle concatenation
-    char sc[200];          // Separate buffer for concatenated string
-
-    // Input two strings
-    scanf("%[^\n]", s1);
-    getchar();             // Consume newline character left in input buffer
-    scanf("%[^\n]", s2);
-
-    // Concatenate s1 with itself into sc
-    strcpy(sc, s1);        // Copy s1 into sc
-    strcat(sc, s1);        // Append s1 to itself in sc
-
-    // Check if s2 is a substring of sc
-    if (strstr(sc, s2)) {
-        printf("Yes\n");
-    } else {
-        printf("No\n");
-    }
-
-    return 0;
+int main()
+{
+    char s1[100],s2[100],sc[100];
+    scanf("%[^\n]",&s1);
+    scanf("\n");
+    scanf("%[^\n]",&s2);
+    scanf("\n");
+    sc=strcat(s1,s1);
+    if(strstr(s1,s2)){printf("Yes");}
+    else{printf("No");}
 }
