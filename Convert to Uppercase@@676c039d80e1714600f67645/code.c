33866bdd7483d9b0e257df4cca582c51c197d74a
Tanishq Tanishq <tanishq130805@gmail.com>
2025-03-20 05:40:23 +0530

    Solution to Convert to Uppercase

---
+++ Convert to Uppercase@@676c039d80e1714600f67645/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    char s[100];
+    scanf("%[^\n]",&s);
+    for(i=0;s[i]!='\0';i++)
+    {
+        if((int)s[i]>90){printf("%c",s[i]-32)}
+        else{printf("%c",s[i]);}
+    }
+}
