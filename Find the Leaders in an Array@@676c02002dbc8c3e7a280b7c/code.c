4de4c7dac6ab713c30a92bcc744146ba80021e83
Tanishq Tanishq <tanishq130805@gmail.com>
2025-03-22 04:48:42 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -13,7 +13,7 @@ int main()
         for(int j=i+1;j<n;j++)
         {
             if(!(arr[i]>=arr[j])){iss=0;break;}
-        }if(iss){printf("%d",arr[i]);}
+        }if(iss){printf("%d ",arr[i]);}
     }
     printf("%d",arr[n-1]);
 
