4b1624d739326816f751ebc6997b6b28e71130ab
Tanishq Tanishq <tanishq130805@gmail.com>
2025-03-28 17:40:52 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <ctype.h>
 void caesarCipher(char message[],int shift,char encrypted[])
 {   int a=(int)'a';
     int last=a+26;
@@ -7,7 +8,7 @@ void caesarCipher(char message[],int shift,char encrypted[])
     {   if(isaplha(message[i]))
     { 
               int k=(int)message[i];
-              if(k+shift<=last){encrypted[i]=(char)(k+shft);}
+              if(k+shift<=last){encrypted[i]=(char)(k+shift);}
               else{encrypted[i]=(char)((k-last)+a);}
     }
     }
