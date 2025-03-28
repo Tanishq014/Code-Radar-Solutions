e2620ea4b07488c3f88a676b231fc82b21f67ceb
Tanishq Tanishq <tanishq130805@gmail.com>
2025-03-28 17:39:09 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,13 @@
+caesarCipher(char message[],int shift,char encrypted[])
+{   int a=(int)'a';
+    int last=a+26;
+    int n=strlen(message);
+    for(int i=0i<n;i++)
+    {   if(isaplha(message[i]))
+    { 
+              int k=(int)message[i];
+              if(k+shift<=last){encrypted[i]=(char)(k+shft);}
+              else{encrypted[i]=(char)((k-last)+a);}
+    }
+    }
+}
