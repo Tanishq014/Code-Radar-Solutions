3e131674fbff7c14c93f122346567067c9e322b1
Tanishq Tanishq <tanishq130805@gmail.com>
2025-04-01 01:38:26 +0530

    Solution to Caesar Cipher Encryption

--- Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -6,11 +6,11 @@ void caesarCipher(char message[],int shift,char encrypted[])
     int n=strlen(message);
     for(int i=0;i<n;i++)
     {   if(isalpha(message[i]))
-    {   int a=islower(message[i])?(int)'a':(int)'A';
-        int last=a+26;
+    {   int aa=islower(message[i])?(int)'a':(int)'A';
+        int last=aa+26;
               int k=(int)message[i];
               if(k+shift<=last){encrypted[i]=(char)(k+shift);}
-              else{encrypted[i]=(char)((k+shift-last)+a);}
+              else{encrypted[i]=(char)((k+shift-last)+aa);}
     }
     else {
             // Fix 3: Handle non-alphabetic characters
