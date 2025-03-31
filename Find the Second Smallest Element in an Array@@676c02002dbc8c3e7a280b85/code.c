1e3fbc7e03825fbb795bc752992c03a5ee0ae6e4
Tanishq Tanishq <tanishq130805@gmail.com>
2025-03-31 21:32:38 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -1,28 +1,50 @@
 #include <stdio.h>
+
 int main()
 {
     int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    int min1=11002,min2=7964;
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i]<min2 && arr[i]>=min1){min2=arr[i];}
-        else if(arr[i]<min2 && arr[i]<min1){int temp=min1;min1=arr[i];min2=temp;}
+    scanf("%d", &n);
 
+    int arr[n];
+
+    // Read array elements
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
     }
-    if(min1==min2)
+
+    if (n < 2) // If there's only one number, no second minimum can exist
+    {
+        printf("-1");
+        return 0;
+    }
+
+    // Initialize min1 and min2 dynamically
+    int min1 = arr[0], min2 = 11002; 
+
+    // Finding the smallest and second smallest element
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] < min1)
+        {
+            min2 = min1;
+            min1 = arr[i];
+        }
+        else if(arr[i] > min1 && arr[i] < min2)
+        {
+            min2 = arr[i];
+        }
+    }
+
+    // If min2 is not updated, there's no second smallest
+    if(min2 == 11002)
     {
         printf("-1");
     }
     else
     {
-        printf("%d",min2);
+        printf("%d", min2);
     }
 
-
+    return 0;
 }
