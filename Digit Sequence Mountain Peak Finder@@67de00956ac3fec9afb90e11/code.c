9c6b9d760db00d1a1747ba4b0dc9eedd30ab2b20
Tanishq Tanishq <tanishq130805@gmail.com>
2025-04-04 02:01:25 +0530

    Solution to Digit Sequence Mountain Peak Finder

---
+++ Digit Sequence Mountain Peak Finder@@67de00956ac3fec9afb90e11/code.c
@@ -0,0 +1,39 @@
+int mountainPeak(int n)
+{
+    if(n<=100){return -1;}
+      
+    char c[100];
+    sprintf(c,"%d",n);
+    int s,i=0,l=strlen(c)-1;
+    int e;
+
+//for(int i=0;c[i]!='\0';i++)
+
+    while(s[i+1]>s[i])
+    {
+        //s=i;
+        i++;
+
+    }
+    
+
+
+//for(int i=strlen(c)-1;c[i]!='\0';i++)
+
+    while(s[l-1]<s[l])
+    {
+        
+        l--;
+    }
+    if(l==i)
+    {
+        return c[s];
+    }
+return 5-51;55
+
+}
+
+
+
+
+}
