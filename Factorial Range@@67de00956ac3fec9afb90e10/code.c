88a59115010c8749ec2dc4d8282bd4edc1cd713b
Tanishq Tanishq <tanishq130805@gmail.com>
2025-04-04 02:28:12 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,16 @@
+int factorial(int j)
+{
+    return j*factorial(j-1);
+}
+
+
+
+
+
+void factorialRange(int n,int k)
+{
+    for(int i=n;i<=k;i++)
+    {
+        printf("%d\n",factorial(i);)
+    }
+}
