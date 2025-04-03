39c1a135d11a960189fd9829911862e2dba2a740
Tanishq Tanishq <tanishq130805@gmail.com>
2025-04-04 01:22:53 +0530

    Solution to Digit Increment to Next Prime

--- Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
+++ Digit Increment to Next Prime@@67de00956ac3fec9afb90e12/code.c
@@ -5,7 +5,7 @@ int change(int n)
     if(n==0||n==1||n==8||n==9){return 2;}
     //else if (n==2||n==4||n==6||)
 }
-int main()
+int incrementToPrimeDigits(int n)
 {   int n;
     scanf("%d",&n);int out=0;
     int count=1;
@@ -15,9 +15,6 @@ int main()
         out=out+(change(j)*count*10);
         count++;
     }
-    printf("%d",out);
-    
-
-
+    return out;
 
 }
