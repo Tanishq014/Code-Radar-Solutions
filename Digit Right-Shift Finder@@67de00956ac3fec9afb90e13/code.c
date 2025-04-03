73cd76facb36625629136bbf9f36fdb1f932d82a
Tanishq Tanishq <tanishq130805@gmail.com>
2025-04-04 01:42:05 +0530

    Solution to Digit Right-Shift Finder

---
+++ Digit Right-Shift Finder@@67de00956ac3fec9afb90e13/code.c
@@ -0,0 +1,16 @@
+int digitRightShift(int n)
+{   
+    int j=n%10;
+    n/=10;
+    int out=0;
+    int count=1;
+    while(n>0)
+    {
+        out+=(count*(n%10));
+        count*=10;
+        n/=10;
+    }
+    out+=(count*10)*j;
+    return out;
+    
+}
