9fc3197116d4e4f232d01c57aa5a85dae88520a0
Tanishq Tanishq <tanishq130805@gmail.com>
2025-04-04 20:50:00 +0530

    Solution to Gaming Tournament – Most Played Game

--- Gaming Tournament – Most Played Game@@67de00956ac3fec9afb90e18/code.c
+++ Gaming Tournament – Most Played Game@@67de00956ac3fec9afb90e18/code.c
@@ -1,13 +1,13 @@
 int mostPlayedGame(int games[], int n) {
-    int freq[100] = {0}; // Assuming game IDs are between 0 and 99
+    int freq[1001] = {0}; // Assuming game IDs are in the range 1 to 1000
     int maxFreq = 0;
-    int result = 0;
+    int result = 1001; // Initialize with a value greater than the maximum possible game ID
 
     // Count frequencies of each game
     for (int i = 0; i < n; i++) {
         freq[games[i]]++;
         if (freq[games[i]] > maxFreq || 
-           (freq[games[i]] == maxFreq && games[i] > result)) {
+           (freq[games[i]] == maxFreq && games[i] < result)) {
             maxFreq = freq[games[i]];
             result = games[i];
         }
