eb2491dc0ed7a98999bbc652ce328a744259366b
Tanishq Tanishq <tanishq130805@gmail.com>
2025-04-07 17:12:53 +0530

    Solution to Find the First Unique Character

---
+++ Find the First Unique Character@@67877c59ddaeab38c8ad25b4/code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char c[100];
+    scanf("%[^\n]",c);
+    int len=strlen(c);
+    for(int i=0;i<len;i++)
+    {int count=0;
+        for(int j=i+1;i<len;j++)
+        {
+            if(c[i]==c[j]){count++;}
+        }
+        if(count==0){printf("%c",c[i]);break;}
+    }
+}
