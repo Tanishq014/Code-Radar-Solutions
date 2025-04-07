4b86eaeb7807b6d49e7f3939fff26804c7bdac8e
Tanishq Tanishq <tanishq130805@gmail.com>
2025-04-07 19:18:48 +0530

    Solution to Topper Tracker – Breaking Test Score Records

--- Topper Tracker – Breaking Test Score Records@@67de00956ac3fec9afb90e1a/code.c
+++ Topper Tracker – Breaking Test Score Records@@67de00956ac3fec9afb90e1a/code.c
@@ -4,7 +4,7 @@ result[0]=0;result[1]=0;
     for(int i=0;i<n;i++)
     {
         if(scores[i]>max){result[0]++;max=scores[i];}
-        else if(scares[i]<low){result[1]++;low=scores[i];}
+        else if(scores[i]<low){result[1]++;low=scores[i];}
     }
     
 }
