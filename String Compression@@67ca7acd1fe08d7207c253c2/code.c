50ee07de2eb22d127dd7e117816d69b07a7b3c3d
Tanishq Tanishq <tanishq130805@gmail.com>
2025-04-10 18:53:55 +0530

    Solution to String Compression

--- String Compression@@67ca7acd1fe08d7207c253c2/code.c
+++ String Compression@@67ca7acd1fe08d7207c253c2/code.c
@@ -11,7 +11,7 @@ void compressString(char str[],char compressed[])
         {count++;}
         else
         {
-            if( count>1 ){compressed[++n]=str[i];compressed[++n]='count';}
+            if( count>1 ){compressed[++n]=str[i];compressed[++n]=(int)count;}
             else{compressed[++n]=str[i];}
         }
     }
